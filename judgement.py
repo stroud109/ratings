d01f039b211198c42e971b0b12c37bf0a8765942
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2013-11-02 17:39:26 -0700

    Made code less hideous

--- judgement.py
+++ judgement.py
@@ -1,25 +1,59 @@
-from flask import Flask, render_template, redirect, request, session, url_for, flash
-import datetime, random
-import model
+import datetime  # built-ins
+import random
+
+from flask import (  # stuff i installed with pip
+    flash,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,  # app session, not DB session
+    url_for,
+)
+
+from model import (  # project imports
+    add_rating,
+    authenticate,
+    Movies,
+    Ratings,
+    session as db_session,
+    User,
+)
+
+MESSAGES = (
+    "I suppose you don't have such bad taste after all.",
+    "I regret every decision that I've ever made that has brought me to listen to your opinion.",
+    "Words fail me, as your taste in movies has clearly failed you.",
+    "That movie is great. For a clown to watch. Idiot.",
+)
+
 app = Flask(__name__)
 app.secret_key = "shhhhhhhhhhhhhhsupersecretthing"
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/search")
 def search():
     search_term = request.args.get("search")
-    search_results = model.session.query(model.Movies).filter(model.Movies.name.like("%"+search_term+"%"))
-    return render_template('search.html', search_results = search_results, search_term = search_term)
+    search_results = db_session.query(Movies) \
+        .filter(Movies.name.like("%"+search_term+"%"))
+    return render_template(
+        'search.html',
+        search_results=search_results,
+        search_term=search_term,
+    )
+
 
 @app.route("/", methods=["POST"])
 def sign_in():
     email = request.form.get("email")
     password = request.form.get("password")
-    user_id = model.authenticate(email, password)
-    if user_id != None:
+    user_id = authenticate(email, password)
+    if user_id is not None:
         flash("loggedin!")
 
         session['user_id'] = user_id
@@ -29,39 +63,57 @@ def sign_in():
         flash("Password or email incorrect. Try again.")
     return redirect(url_for("index"))
 
+
 @app.route("/users")
 def view_all_users():
-    user_list = model.session.query(model.User).limit(20).all()
-    return render_template("user_list.html", users = user_list)
+    user_list = db_session.query(User).limit(20).all()
+    return render_template("user_list.html", users=user_list)
+
 
 @app.route("/users/<user_id>")
 def view_user_ratings(user_id):
-    user_ratings = model.session.query(model.Ratings).filter_by(user_id = user_id).all()
-    return render_template("user_ratings.html", ratings = user_ratings)
+    user_ratings = db_session.query(Ratings).filter_by(user_id=user_id).all()
+    return render_template("user_ratings.html", ratings=user_ratings)
+    #display the best_movies
+
 
 @app.route("/movies")
 def view_all_movies():
     random_start = random.randint(0, 1500)
     random_end = random_start + 100
-    movie_list = model.session.query(model.Movies).filter(model.Movies.id > random_start, model.Movies.id < random_end).all()
-    return render_template("movies.html", movies = movie_list)
+    movie_list = db_session.query(Movies).filter(
+        Movies.id > random_start,
+        Movies.id < random_end,
+    ).all()
+    return render_template("movies.html", movies=movie_list)
+    # movies gets passed to the html template,
+    # movie_list refers to the attribute within this function
+
 
 @app.route("/movies/<movie_id>")
 def view_movie(movie_id):
-    movie_details = model.session.query(model.Ratings).filter_by(movie_id=movie_id).all()
-    movie = model.session.query(model.Movies).filter_by(id = movie_id).one()
+    movie_details = db_session.query(Ratings).filter_by(
+        movie_id=movie_id,
+    ).all()
+    movie = db_session.query(Movies).filter_by(id=movie_id).one()
     user_id = session.get('user_id')
-    the_eye = model.session.query(model.User).filter_by(id=946).one()
+    the_eye = db_session.query(User).filter_by(id=946).one()
     prediction = None
     eye_rating = None
-    if user_id:
-        rating = model.session.query(model.Ratings).filter_by(movie_id = movie_id, user_id = user_id).all()
+    if user_id is not None:
+        rating = db_session.query(Ratings).filter_by(
+            movie_id=movie_id,
+            user_id=user_id,
+        ).all()
         if not rating:
-            user = model.session.query(model.User).filter_by(id = user_id).one()
+            user = db_session.query(User).filter_by(id=user_id).one()
             prediction = user.rating_prediction(movie)
             effective_rating = prediction
 
-            eye_rating = model.session.query(model.Ratings).filter_by(user_id = the_eye.id, movie_id = movie.id).first()
+            eye_rating = db_session.query(Ratings).filter_by(
+                user_id=the_eye.id,
+                movie_id=movie.id,
+            ).first()
         else:
             effective_rating = rating[0].rating
 
@@ -72,25 +124,32 @@ def view_movie(movie_id):
 
         difference = abs(eye_rating - effective_rating)
 
-        messages = [ "I suppose you don't have such bad taste after all.",
-             "I regret every decision that I've ever made that has brought me to listen to your opinion.",
-             "Words fail me, as your taste in movies has clearly failed you.",
-             "That movie is great. For a clown to watch. Idiot.",]
+        beratement = MESSAGES[int(difference)]
 
-        beratement = messages[int(difference)]
+    return render_template(
+        "movie_ratings.html",
+        details=movie_details,
+        rating=rating,
+        prediction=prediction,
+        beratement=beratement,
+    )
 
-    return render_template("movie_ratings.html", details = movie_details, rating = rating, prediction = prediction, beratement = beratement)
 
 @app.route("/movies/<movie_id>", methods=["POST"])
 def rate_movie(movie_id):
     print "rating movie"
     rating = request.form.get("ratingRadio")
     if rating:
-        new_rating = model.Ratings(movie_id = movie_id, user_id = session.get("user_id"), timestamp = datetime.datetime.now(), rating = rating)
-        model.add_rating(new_rating) #add + commit
+        new_rating = Ratings(
+            movie_id=movie_id,
+            user_id=session.get("user_id"),
+            timestamp=datetime.datetime.now(),
+            rating=rating,
+        )
+        add_rating(new_rating)  # add + commit
 
         flash("Your rating has been recorded!")
-    return redirect(url_for("view_movie", movie_id = movie_id))
+    return redirect(url_for("view_movie", movie_id=movie_id))
 
 
 @app.route("/clear")
@@ -99,4 +158,4 @@ def session_clear():
     return redirect(url_for("index"))
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,28 +1,40 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Date, DateTime
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship, backref
 import correlation
+
+from sqlalchemy import (
+    Column,
+    create_engine,
+    Date,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import (
+    backref,
+    relationship,
+    scoped_session,
+    sessionmaker,
+)
 
 engine = create_engine("sqlite:///ratings.db", echo=False)
 session = scoped_session(sessionmaker(bind=engine,
-                                      autocommit = False,
-                                      autoflush = False))
+                                      autocommit=False,
+                                      autoflush=False,))
 
 Base = declarative_base()
 Base.query = session.query_property()
 
+
 ### Class declarations go here
-class User(Base): # child
+class User(Base):  # child
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key = True)
-    email = Column(String(64), nullable = True)
-    password = Column(String(64), nullable = True)
-    age = Column(Integer, nullable = True)
-    zipcode = Column(String(15), nullable = True)
+    id = Column(Integer, primary_key=True)
+    email = Column(String(64), nullable=True)
+    password = Column(String(64), nullable=True)
+    age = Column(Integer, nullable=True)
+    zipcode = Column(String(15), nullable=True)
 
     def similarity(self, other):
         u_ratings = {}
@@ -44,57 +56,74 @@ class User(Base): # child
 
     def rating_prediction(self, movie):
         other_ratings = movie.ratings
-        similarities = [ (self.similarity(rating.user), rating) for rating in other_ratings ]
+        similarities = [(self.similarity(r.user), r) for r in other_ratings]
         similarities.sort(reverse=True)
-        pos_sim = [ s for s in similarities if s[0] > 0]
+        # we're creating a list of tuples where (pearson's similarity, rating)
+        pos_sim = [s for s in similarities if s[0] > 0]
         if not pos_sim:
             return None
         numerator = sum([r.rating * similarity for similarity, r in pos_sim])
-        denominator = sum([similarity[0] for similarity in pos_sim])
-        print numerator, denominator
-        print numerator/denominator
+        # similarity,r unpacks a tuple in the similarities list we just created
+        # r represents the instance of the class Rating
+        # rating represents an attribute of the instance of Rating
+        # rating represents the actual number from the DB
+        denominator = sum([similarity for similarity, r in pos_sim])
         return numerator/denominator
 
-class Movies(Base): # parent
+    def best_movies(self):  # best movies (predictions) for this user
+        pass  # something something
+
+        # return recs for movies user hasn't rated based on predictions
+        # display movie names in the controller
+        # display this on the user/<user_id> page
+
+
+class Movies(Base):  # parent
     __tablename__ = "movies"
 
-    id = Column(Integer, primary_key = True)
-    name = Column(String(64), nullable = True)
-    released_at = Column(Date(), nullable = True)
-    #DD-Mon-YYYY
-    imdb_url = Column(String(300), nullable = True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=True)
+    released_at = Column(Date(), nullable=True)
+    # DD-Mon-YYYY
+    imdb_url = Column(String(300), nullable=True)
 
 
-class Ratings(Base): # association
+class Ratings(Base):  # association
     __tablename__ = "ratings"
-    id = Column(Integer, primary_key = True)
-    movie_id = Column(Integer(), ForeignKey('movies.id'), nullable = True)
-    user_id = Column(Integer(), ForeignKey('users.id'), nullable = True)
-    timestamp = Column(DateTime(), nullable = True)
-    #unix seconds since 1/1/1970
-    rating = Column(Integer(), nullable = True)
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer(), ForeignKey('movies.id'), nullable=True)
+    user_id = Column(Integer(), ForeignKey('users.id'), nullable=True)
+    timestamp = Column(DateTime(), nullable=True)
+    # unix seconds since 1/1/1970
+    rating = Column(Integer(), nullable=True)
 
-    user = relationship("User",
-                        backref=backref("ratings", order_by=id))
-    movies = relationship("Movies",
-                        backref=backref("ratings", order_by=id))
+    user = relationship(
+        "User",
+        backref=backref("ratings", order_by=id),
+    )
+    movies = relationship(
+        "Movies",
+        backref=backref("ratings", order_by=id),
+    )
 
 
 ### End class declarations
 
 def authenticate(in_email, in_password):
     in_password = hash(in_password)
-    user = session.query(User).filter_by(email = in_email, password = in_password).first()
-    if user != None:
+    user = session.query(User).filter_by(
+        email=in_email,
+        password=in_password,
+    ).first()
+    if user is not None:
         return user.id
     else:
         return None
 
+
 def add_rating(new_rating):
     session.add(new_rating)
     session.commit()
-
-
 
     # users = []
     # for other_u in other_users:
@@ -108,6 +137,7 @@ def add_rating(new_rating):
     # top_user_rating = top_user_rating.rating
     # predicted_rating = top_user[0] * top_user_rating
     # return predicted_rating
+
 
 def main():
     """In case we need this for something"""
